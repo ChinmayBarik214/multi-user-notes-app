7ec1707752b2eefa6d35b1f1b353b53c45b90c2e
Chinmay <chinmaybarik214@gmail.com>
2025-03-03 11:13:19 +0530

    day 2: march 3 2025

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 })
-
+console.log(process.env.MONGO_URI)
 app.listen(5000, () => {
   console.log("Server started at 5000 hello");
 });
+// npm run dev
