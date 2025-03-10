570cf2c43068163c55a9744ea2285f910fe5f397
Chinmay <chinmaybarik214@gmail.com>
2025-03-10 14:45:56 +0530

    delete route created

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,16 @@ app.post("/api/notes", async (req, res) => {
   }
 });
 
+app.delete("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Note.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Note deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Note not found" });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server started at 5000 hello");
