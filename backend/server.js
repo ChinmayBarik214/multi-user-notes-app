7e8ad5711e681a3f0cf6f4ef616384a2d47f316b
Chinmay <chinmaybarik214@gmail.com>
2025-03-10 14:32:27 +0530

    reformat

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,18 @@ app.post("/api/notes", async (req, res) => {
   const note = req.body; // user will send this data
 
   if (!note.title || !note.description || !note.color) {
-    return res.status(400).json({success: false, message: "Please provide all fields"})
-  } 
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
   const newNote = new Note(note);
-  
+
   try {
     await newNote.save();
-    res.status(201).json({success: true, data: newNote});
+    res.status(201).json({ success: true, data: newNote });
   } catch (error) {
     console.error("Error in create product", error.message);
-    res.status(500).json({success: false, message: "Server error"})
+    res.status(500).json({ success: false, message: "Server error" });
   }
 });
 
